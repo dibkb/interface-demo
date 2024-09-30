53340e3ab7655b9e0a987cd0111e9b1e32602268
dibkb <dibas9110@gmail.com>
2024-09-30 22:32:32 +0530

    layout setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
 export default function Home() {
-  return <div className="">Hello</div>;
+  return (
+    <div className="grid grid-cols-12 grid-rows-[auto_1fr] min-h-screen">
+      <aside className="col-span-2 row-span-2 grid grid-rows-[auto_1fr]">
+        <div className="h-[4.25rem] border-t border-r border-b py-5 px-8">
+          Aside Row 1
+        </div>
+        <div className="border-r p-6">Aside Row 3 (Full height)</div>
+      </aside>
+
+      <main className="col-span-10 row-span-2 grid grid-rows-[auto_1fr]">
+        <div className="h-[4.25rem] border-t border-b py-5 px-8">
+          Main Row 1
+        </div>
+        <div className="p-6">Main Row 2 (Full height)</div>
+      </main>
+    </div>
+  );
 }
