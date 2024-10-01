dc0589c21107b28c35f8eb3b439404a759c35f73
dibkb <dibas9110@gmail.com>
2024-10-01 14:56:32 +0530

    grid layout done

--- components/Logs/Mainpage.tsx
+++ components/Logs/Mainpage.tsx
@@ -2,13 +2,23 @@ import React from "react";
 
 const Mainpage = () => {
   return (
-    <div className="border h-full overflow-y-auto">
-      <div className="h-56 bg-red-300">dfdsfs</div>
-      <div className="h-56 bg-red-300">dfdsfs</div>
-      <div className="h-56 bg-red-300">dfdsfs</div>
-      <div className="h-56 bg-red-300">dfdsfs</div>
-      <div className="h-56 bg-red-300">dfdsfs</div>
-      <div className="h-56 bg-red-300">dfdsfs</div>
+    <div className="border h-full overflow-y-auto grid grid-cols-12">
+      <div className="border-r col-span-3 flex flex-col h-full">
+        <span className="h-16 border-b"></span>
+        <div className="flex-1 overflow-y-auto"></div>
+      </div>
+      <div className="border-r col-span-3 flex flex-col h-full">
+        <span className="h-16 border-b"></span>
+        <div className="flex-1 overflow-y-auto"></div>
+      </div>
+      <div className="border-r col-span-3 flex flex-col h-full">
+        <span className="h-16 border-b"></span>
+        <div className="flex-1 overflow-y-auto"></div>
+      </div>
+      <div className="col-span-3 flex flex-col h-full">
+        <span className="h-16 border-b"></span>
+        <div className="flex-1 overflow-y-auto"></div>
+      </div>
     </div>
   );
 };
