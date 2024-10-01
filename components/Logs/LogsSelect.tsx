c78d7686b760a3989ad268a0e48ddb7cb0084a2a
dibkb <dibas9110@gmail.com>
2024-10-01 20:06:56 +0530

    log selector

--- components/Logs/LogsSelect.tsx
+++ components/Logs/LogsSelect.tsx
@@ -8,12 +8,12 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { logMessages, logMessagesDisplay } from "@/constants/log-messages";
-import { useRouter } from "next/navigation";
-import { useCallback } from "react";
+import { useParams, useRouter } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
 
 export function LogsSelect() {
   const router = useRouter();
-
+  const [type, setType] = useState<string>("");
   const handleValueChange = useCallback(
     (key: string) => {
       router.push(
@@ -23,9 +23,16 @@ export function LogsSelect() {
     [router]
   );
 
+  const params = useParams();
+  useEffect(() => {
+    if (params?.type === undefined) {
+      setType(params.type);
+    }
+  }, [params]);
+
   return (
-    <Select onValueChange={handleValueChange}>
-      <SelectTrigger className="w-[150px] border-none bg-transparent shadow-none font-bold text-interface-base-black">
+    <Select onValueChange={handleValueChange} value={type}>
+      <SelectTrigger className="w-[160px] border-none bg-transparent shadow-none font-bold text-interface-base-black">
         <SelectValue placeholder="Select a type" />
       </SelectTrigger>
       <SelectContent>
