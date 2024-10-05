33feb35c684400dc7287881e45f905ea007d85cf
dibkb <dibas9110@gmail.com>
2024-10-05 12:00:20 +0530

    svg error

--- components/icons/Pencil.tsx
+++ components/icons/Pencil.tsx
@@ -15,15 +15,15 @@ const Pencil = (props: SVGProps) => {
         <path
           d="M14.0774 4.86834C14.6614 4.28704 15.6057 4.28814 16.1884 4.87079L17.8162 6.49855C18.3987 7.08112 18.3999 8.02527 17.8189 8.60932L8.46227 18.0137L4.67309 18.0137L4.67309 14.229L14.0774 4.86834Z"
           stroke="#888888"
-          stroke-width="1.38158"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.38158"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M12.8896 6.05786L16.6257 9.79395"
           stroke="#888888"
-          stroke-width="1.38158"
-          stroke-linecap="round"
+          strokeWidth="1.38158"
+          strokeLinecap="round"
         />
       </svg>
     </div>
